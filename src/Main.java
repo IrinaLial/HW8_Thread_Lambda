5ebc151a560cb90a9fbdbd0131f083c331aea110
Iryna Lialenko <lyalenko.iryna@gmail.com>
2017-11-21 23:08:10 +0200

    add class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import library.Generator;
 public class Main {
     public static void main ( String[] args ) {
         Library library= new Library();
-        library.library (Generator.generateUsers(20));
+        library.library (Generator.generateUsers(5));
+
     }
 }
